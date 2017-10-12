fac256ed38d7306bd1220e66f95f2251e953f559
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2017-10-12 13:26:44 -0700

    Finished the assignment

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- Import FormsModule
+import { HttpModule } from '@angular/http'; // <-- Import HttpModule
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule, // <-- Include module in our AppModules
+    HttpModule // <-- Include module in our AppModules
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
